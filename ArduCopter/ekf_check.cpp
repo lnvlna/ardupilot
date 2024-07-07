17dcc0c8bea421132855db84db81e309a74fdfd1
lnvlna <42850441+lnvlna@users.noreply.github.com>
2024-07-07 12:46:00 +0300

    Update ekf_check.cpp

--- ArduCopter/ekf_check.cpp
+++ ArduCopter/ekf_check.cpp
@@ -191,13 +191,13 @@ void Copter::failsafe_ekf_event()
         case FS_EKF_ACTION_ALTHOLD:
             // AltHold
             if (failsafe.radio || !set_mode(Mode::Number::ALT_HOLD, ModeReason::EKF_FAILSAFE)) {
-                set_mode_land_with_pause(ModeReason::EKF_FAILSAFE);
+                set_mode_RTL_or_land_with_pause(ModeReason::EKF_FAILSAFE);
             }
             break;
         case FS_EKF_ACTION_LAND:
         case FS_EKF_ACTION_LAND_EVEN_STABILIZE:
         default:
-            set_mode_land_with_pause(ModeReason::EKF_FAILSAFE);
+            set_mode_RTL_or_land_with_pause(ModeReason::EKF_FAILSAFE);
             break;
     }
 
